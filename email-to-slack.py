2dab26f708fd673f822c7aff83fcf19da3b44340
Douglas Land <dsl@looprock.com>
2015-11-02 09:24:17 -0600

    added options to make email script more flexible

--- email-to-slack.py
+++ email-to-slack.py
@@ -6,24 +6,72 @@ import sys
 import email
 import requests
 import json
+import getopt
+import logging
+
+logging.basicConfig()
+
+
+def usage():
+    print sys.argv[0] + """ [options] [argument]
+    options:
+
+    -h/--help         -  print usage summary
+    -d/--debug        -  print debug output
+    -s/--subjectonly  -  only send the subject of the email, not the entire body
+    -t/--topic=       -  specify the topic (default: email)
+    -c/--channel=     -  send to a particular channel via the gateway (default: no channel specified)
+    -g/--gateway=     -  specify the gateway ip/hostname (default: 127.0.0.1)
+    -p/--gatewayport= -  specify the gateway port (default:18080)
+"""
 
 # setup
 # put this script somewhere
 # configure the URL parameter below
 # install this script on a local or remote system with a mail server
 # add to /etc/aliases:
-# slack: "|/path/to/email-to-slack.py"
+# slack: "|/path/to/email-to-slack.py <options>"
 # run newaliases
 # you're done. Now sending mail to slack@host should send it through the gateway
 
-# slack gateway
-url = "http://[your_host_here]:18080"
-debug = True
+debug = False
+gateway = "127.0.0.1"
+gatewayport = "18080"
+subjectonly = False
+topic = "email"
+channel = ""
+
+try:
+    opts, remainder = getopt.gnu_getopt(sys.argv[1:], "hdsc:t:g:p:", ["help", "debug", "subjectonly", "channel=", "topic=", "gateway=", "gatewayport="])
+except getopt.GetoptError:
+    usage()
+    sys.exit(2)
+for opt, arg in opts:
+    if opt in ("-h", "--help"):
+        usage()
+        sys.exit()
+    elif opt in ("-d", "--debug"):
+        debug = True
+    elif opt in ("-s", "--subject-only"):
+        subjectonly = True
+    elif opt in ("-t", "--topic"):
+        topic = arg
+    elif opt in ("-c", "--channel"):
+        channel = arg
+    elif opt in ("-g", "--gateway"):
+        gateway = arg
+    elif opt in ("-p", "--gatewayport"):
+        gatewayport = arg
 
 
 def degbu(string):
     if debug:
         print "DEBUG: %s" % string
+
+
+# slack gateway
+url = "http://%s:%s/%s" % (gateway, gatewayport, channel)
+degbu("Url: %s" % url)
 
 
 def sanitize(txt):
@@ -59,16 +107,19 @@ else:
 
 load = sanitize(load.rstrip("\n"))
 
-data = """Message date: %s\n
-Message from: %s\n
-Message subject: %s\n
-Message body:\n
-%s\n\n""" % (msgdate, msgfrom, msgsubject, load)
+if subjectonly:
+    data = msgsubject
+else:
+    data = """Message date: %s\n
+    Message from: %s\n
+    Message subject: %s\n
+    Message body:\n
+    %s\n\n""" % (msgdate, msgfrom, msgsubject, load)
 
 if debug:
     print data
 
-payload = {"topic": "email", "message": data}
+payload = {"topic": topic, "message": data}
 degbu("payload:")
 if debug:
     print payload
